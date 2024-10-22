2126ff73fe55d11b80076d2668a16ea87624bc50
Max R <maxrerisi+git@gmail.com>
2024-10-22 17:53:24 -0400

    Test with easy python script to auto push and stuff test

--- notStupidTest.py
+++ notStupidTest.py
@@ -7,4 +7,5 @@ commit_msg = "Test with easy python script to auto push and stuff"
 os.system("git add .")
 os.system(f'git commit -m "{commit_msg} test"')
 os.system("git push")
-os.system(r"echo ^<ESC^>[32m ^[[32mPushed Successfully.^[[0m")
+
+print("\033[32mPushed Successfully.\033[0m")  # Green text
