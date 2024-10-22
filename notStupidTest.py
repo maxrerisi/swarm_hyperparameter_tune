5513ad23177f6aeb5192fb5e40071e7c348000b1
Max R <maxrerisi+git@gmail.com>
2024-10-22 17:47:33 -0400

    Test with easy python script to auto push and stuff test

--- notStupidTest.py
+++ notStupidTest.py
@@ -7,3 +7,4 @@ commit_msg = "Test with easy python script to auto push and stuff"
 os.system("git add .")
 os.system(f'git commit -m "{commit_msg} test"')
 os.system("git push")
+os.system("type pushMessage.txt")
